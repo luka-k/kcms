42b558fb4a60a1262643674175e9c199d0dfd58f
luka-k <luka-rabota@yandex.ru>
2014-09-11 16:16:32 +0400

    вынес обновление html в отдельную функцию

--- template/client/js/cart.js
+++ template/client/js/cart.js
@@ -2,10 +2,7 @@ function add_to_cart(item_id){
 	data = new Object();
 	data.item_id = item_id;
 	var json_str = JSON.stringify(data);
-	$.post ("/ajax/add_to_cart/", json_str, function(res) {
-		$('#total_qty').text(res['total_qty']);
-		$('#total_price').text(res['total_price']);
-	}, "json");
+	$.post ("/ajax/add_to_cart/", json_str, update_items, "json");
 }
 	
 function update_cart(item_id, qty){
@@ -13,11 +10,7 @@ function update_cart(item_id, qty){
 	data.item_id = item_id;
 	data.qty = qty;
 	var json_str = JSON.stringify(data);
-	$.post ("/ajax/update_cart/", json_str, function(res) {
-		$('#total_qty').text(res['total_qty']);
-		$('#total_price').text(res['total_price']);
-		$('#'+item_id).text(res['item_total']);
-	}, "json");
+	$.post ("/ajax/update_cart/", json_str, update_items, "json");
 }
 	
 function delete_item(item_id){
@@ -25,8 +18,11 @@ function delete_item(item_id){
 	data = new Object();
 	data.item_id = item_id;
 	var json_str = JSON.stringify(data);
-	$.post ("/ajax/delete_item/", json_str, function(res) {
-		$('#total_qty').text(res['total_qty']);
-		$('#total_price').text(res['total_price']);
-	}, "json");
+	$.post ("/ajax/delete_item/", json_str, update_items, "json");
+}
+
+function update_items(res){		
+	$('#total_qty').text(res['total_qty']);
+	$('#total_price').text(res['total_price']);
+	$('#'+item_id).text(res['item_total']);
 }
