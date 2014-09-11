0623cbd1a53e9d02354f5726372d1b6a451deedc
luka-k <luka-rabota@yandex.ru>
2014-09-11 14:55:43 +0400

    вернул пропавшее удаление элемента через ajax

--- application/views/client/cart.php
+++ application/views/client/cart.php
@@ -1,19 +1,4 @@
 <? require 'include/head.php' ?>
-
-<script>
-	function update_cart(item_id){
-		data = new Object();
-		data.item_id = item_id;
-		data.qty = $('#qty_'+item_id).val();
-		var json_str = JSON.stringify(data);
-		$.post ("/ajax/update_cart/", json_str, function(res) {
-			
-			$('#total_qty').text(res['total_qty']);
-			$('#total_price').text(res['total_price']);
-			$('#item_total_'+item_id).text(res['item_total']);
-		}, "json");
-	}
-</script>
 	<div class="grid flex">
 		<div id="menu col_12">
 			<? require 'include/top-menu.php'?>
@@ -35,13 +20,13 @@
 							<tbody>
 								<?$counter = 1?>
 								<?foreach($cart as $item_id => $item):?>
-									<tr>
+									<tr id="tr_<?=$item_id?>">
 										<td><?=$counter?></td>
 										<td><?=$item['title']?></td>
 										<td><?=$item['price']?></td>
 										<td><input type="text" name="qty_<?=$item_id?>" id="qty_<?=$item_id?>" value="<?=$item['qty']?>" onchange="update_cart('<?=$item_id?>', this.value);"/></td>
 										<td><span id="item_total_<?=$item_id?>"><?=$item['item_total']?></span></td>
-										<td><a href="<?=base_url()?>/cart/delete_item/<?=$item_id?>"><i class="icon-minus-sign icon-2x"></i></a></td>
+										<td><a href="#" onclick="delete_item('<?=$item_id?>');"><i class="icon-minus-sign icon-2x"></i></a></td>
 									<tr>
 									<?$counter++?>
 								<?endforeach;?>
