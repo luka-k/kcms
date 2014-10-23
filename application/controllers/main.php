cfdde7fbcc78bfe3a2133a19203d285802025173
luka-k <luka-rabota@yandex.ru>
2014-10-23 14:39:04 +0400

    warning

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -13,12 +13,21 @@ class Main extends CI_Controller {
 	{		
 		$settings = $this->settings->get_item_by(array('id' => 1));
 		$top_menu = $this->menus->top_menu;
+		
+		$cart = $this->cart->get_all();
+		$total_price = $this->cart->total_price();
+		$total_qty = $this->cart->total_qty();
+		
 		$data = array(
 			'title' => $settings->site_title,
 			'meta_title' => $settings->site_title,
 			'meta_keywords' => $settings->site_keywords,
 			'meta_description' => $settings->site_description,
-			'top_menu' => $top_menu
+			'top_menu' => $top_menu,
+			'cart' => $cart,
+			'total_price' => $total_price,
+			'total_qty' => $total_qty,
+			'product_word' => end_maker("товар", $total_qty)
 		);
 		$this->load->view('client/main.php', $data);
 	}	
