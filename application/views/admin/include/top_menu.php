954e5658139832e67b7e105b32ba27843be2a8c9
l <luka-rabota@yandex.ru>
2014-11-08 00:43:54 +0300

    ссылка на пользователя в верхнем меню

--- application/views/admin/include/top_menu.php
+++ application/views/admin/include/top_menu.php
@@ -12,7 +12,7 @@
 			</li>
 		<?endforeach;?>
 		<li class="right"><a href="<?=base_url()?>registration/do_exit"><i class="icon-remove"></i> Выйти</a></li>
-		<li class="right"><a href="<?=base_url()?>/registration/user/<?=$user_id?>"><i class="icon-user"></i><?=$user_name?></a></li>
+		<li class="right"><a href="<?=base_url()?>admin/item/users/<?=$user_id?>"><i class="icon-user"></i><?=$user_name?></a></li>
 		<li class="right"><a href="<?=base_url()?>" target = "_blanc"><i class="icon-signout"></i> Перейти на сайт</a></li>
 	</ul>
 </div>	
