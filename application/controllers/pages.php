6f0b17616d5030382019f8631ed30e3b7d113321
l <luka-rabota@yandex.ru>
2014-11-18 23:07:42 +0300

    pages extends Client_controller

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Pages extends CI_Controller {
+class Pages extends Client_Controller {
 
 	public function __construct()
 	{
@@ -9,17 +9,15 @@ class Pages extends CI_Controller {
 	
 	public function index()
 	{
-		$top_menu = $this->menus->top_menu;
 		$left_menu = $this->dynamic_menus->get_menu(4);
 		
 		$data = array(
 			'tree' => $this->categories->get_site_tree(0, "parent_id"),
-			'top_menu' => $top_menu,
+			'top_menu' => $this->top_menu,
 			'left_menu' => $left_menu,
 		);
 		
 		$page = $this->articles->url_parse(2);
-		//var_dump($page);
 		if(isset($page->article))
 		{
 			$content = $page->article;
@@ -28,7 +26,6 @@ class Pages extends CI_Controller {
 		else
 		{
 			$content = $this->articles->get_list(array("parent_id" => $page->id));
-			//var_dump($content);
 			$content = $this->articles->get_prepared_list($content);
 			$template="client/articles.php";
 		}
