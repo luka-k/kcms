e5eb50fc821e072aa2c9603479076c6c87e7ec03
luka-k <luka-rabota@yandex.ru>
2014-12-22 17:42:28 +0300

    глючная навигация в футере

--- application/views/client/include/footer-nav.php
+++ application/views/client/include/footer-nav.php
@@ -3,12 +3,12 @@
 		<?foreach($top_menu as $level_1):?>
 			<ul class="footer-nav__list">
 				<li class="footer-nav__item">
-					<a href="<?=$level_1->url?>" class="footer-nav__href footer-nav__title skew"><?=$level_1->name?></a>
+					<a href="<?=$level_1->full_url?>" class="footer-nav__href footer-nav__title skew"><?=$level_1->name?></a>
 				</li> <!-- /.footer-nav__item -->
 				<?if($level_1->childs):?>
 					<?foreach($level_1->childs as $level_2):?>
 						<li class="footer-nav__item">
-							<a href="<?=$level_2->url?>" class="footer-nav__href"><?=$level_2->name?></a>
+							<a href="<?=$level_2->full_url?>" class="footer-nav__href"><?=$level_2->name?></a>
 						</li> <!-- /.footer-nav__item -->
 					<?endforeach;?>
 				<?endif;?>
