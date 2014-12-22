51d1770d1133e637a13c28f1487fc7ca488ad614
luka-k <luka-rabota@yandex.ru>
2014-12-22 17:49:11 +0300

    третий уровень в меню футера

--- application/views/client/include/footer-nav.php
+++ application/views/client/include/footer-nav.php
@@ -9,10 +9,19 @@
 					<?foreach($level_1->childs as $level_2):?>
 						<li class="footer-nav__item">
 							<a href="<?=$level_2->full_url?>" class="footer-nav__href"><?=$level_2->name?></a>
+							<?if($level_2->childs):?>
+								<ul class="footer-nav-level-2">
+									<?foreach($level_2->childs as $level_3):?>
+										<li class="footer-nav-level-2__item">
+											<a href="<?=$level_3->full_url?>" class="footer-nav-level-2__href"><?=$level_3->name?></a>
+										</li> <!-- /.footer-nav-level-2__item -->
+									<?endforeach;?>
+								</ul>
+							<?endif;?>
 						</li> <!-- /.footer-nav__item -->
 					<?endforeach;?>
 				<?endif;?>
 			</ul>
 		<?endforeach;?>
 	</div> <!-- /.footer-nav -->
-</div> <!-- /.footer__nav -->   
+</div> <!-- /.footer__nav --> 
