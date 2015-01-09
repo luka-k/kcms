c6ad8a2f43502351150d68ae483f0264e1ca3f84
luka-k <luka-rabota@yandex.ru>
2015-01-09 15:05:12 +0300

    прямой переход на страницу товара из autocomplete

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -9,40 +9,50 @@ class Search extends Client_Controller {
 	
 	public function index()
 	{
-	
-		$this->breadcrumbs->add("catalog", "Каталог");
-		$this->breadcrumbs->add("", "Поиск");
-		
 		$name = $this->input->get('q');
-		$this->db->like('name', $name);
-		$query = $this->db->get('products');
 		
-		$products = array();
-		$pnames = array();
-		foreach ($query->result() as $row)
+		$product = $this->products->get_item_by(array("name" => $name));
+		
+		if(!empty($product))
 		{
-			if (!$pnames[$row->name])
-				$products[] = $row;
-			$pnames[$row->name] = 1;
-		}	
+			$product = $this->products->prepare($product);
+			redirect($product->full_url);
+		}
+		else
+		{
+			$this->breadcrumbs->add("catalog", "Каталог");
+			$this->breadcrumbs->add("", "Поиск");
 		
-		$settings = $this->settings->get_item_by(array('id' => 1));
+			$this->db->like('name', $name);
+			$query = $this->db->get('products');
+		
+			$products = array();
+			$pnames = array();
+			foreach ($query->result() as $row)
+			{
+				if (!$pnames[$row->name])
+					$products[] = $row;
+				$pnames[$row->name] = 1;
+			}	
+		
+			$settings = $this->settings->get_item_by(array('id' => 1));
 
-		$data = array(
-			'title' => $settings->site_title,
-			'meta_title' => $settings->site_title,
-			'meta_keywords' => $settings->site_keywords,
-			'meta_description' => $settings->site_description,
-			'breadcrumbs' => $this->breadcrumbs->get(),
-			'tree' => $this->categories->get_site_tree(0, "parent_id"),
-			'cart_items' => $this->cart_items,
-			'total_price' => $this->total_price,
-			'total_qty' => $this->total_qty,
-			'product_word' => end_maker("товар", $this->total_qty),
-			'top_menu' => $this->menus->set_active($this->top_menu, 'catalog'),
-			'user' => $this->users->get_item_by(array("id" => $this->user_id)),
-			'products' => $this->products->get_prepared_list($products)
-		);
-		$this->load->view("client/products", $data);
+			$data = array(
+				'title' => $settings->site_title,
+				'meta_title' => $settings->site_title,
+				'meta_keywords' => $settings->site_keywords,
+				'meta_description' => $settings->site_description,
+				'breadcrumbs' => $this->breadcrumbs->get(),
+				'tree' => $this->categories->get_site_tree(0, "parent_id"),
+				'cart_items' => $this->cart_items,
+				'total_price' => $this->total_price,
+				'total_qty' => $this->total_qty,
+				'product_word' => end_maker("товар", $this->total_qty),
+				'top_menu' => $this->menus->set_active($this->top_menu, 'catalog'),
+				'user' => $this->users->get_item_by(array("id" => $this->user_id)),
+				'products' => $this->products->get_prepared_list($products)
+			);
+			$this->load->view("client/products", $data);
+		}
 	}
 }
