c59b198efb9bcb755c83125067670c5f416b1b45
Stas Kaiava <metadiel@gmail.com>
2015-01-17 18:31:53 +0300

    удалалалить

--- application/views/admin/include/delete_popup.php
+++ application/views/admin/include/delete_popup.php
@@ -2,7 +2,7 @@
 <div id="delete_item" style="display:none;">
 	<div class="pop-up">
 		<div>
-			Вы точно уверены что хотите удалалить - <strong id="item_name"></strong>?
+			Вы точно уверены, что хотите удалить - <strong id="item_name"></strong>?
 		</div><br/>
 		<a href="" class="delete_button button small">Удалить?</a>
 		<a href="#" class="button small" onclick="$.fancybox.close();">Нет</a>
