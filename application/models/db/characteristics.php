67f6fba5e239da278df4a41ec84dee6cbe684283
luka-k <luka-rabota@yandex.ru>
2015-02-01 00:43:06 +0300

    db/characteristics

--- application/models/db/characteristics.php
+++ application/models/db/characteristics.php
@@ -44,4 +44,91 @@ class Characteristics extends MY_Model
 		}
 		return $filters;
 	}
+	
+	function get_filtred($get, $order, $direction, $limit = FALSE, $from = FALSE)
+	{
+		$values = array();
+		$filters = $this->characteristics->filters;
+		$counter = 0;
+		foreach($get as $key => $item)
+		{
+			if(!empty($item)) 
+			{
+				if(isset($filters[$key]))
+				{
+					switch ($filters[$key][1])
+					{
+						case "text":
+							$this->db->where(array("type" => $key, "value" => $item));
+							$values = $this->_update_values($values);
+							$counter++;
+							break;
+						case "multy":
+							$this->db->where("type", $key);
+							$this->db->where_in("value", $item);
+							$counter++;
+							break;
+						case "single":
+							$this->db->where(array("type" => $key, "value" => $item));
+							$counter++;
+							break;
+						case "interval":
+							if(!empty($item[0])&&!empty($item[1]))
+							{
+								$this->db->where("type", $key);
+								$where = "{$name} BETWEEN {$item[0]} AND {$item[1]}";
+								$this->db->where($where);
+								$counter++;
+							}
+							elseif(!empty($item[0])||!empty($item[1]))
+							{
+								$this->db->where("type", $key);
+								if(!empty($item[0])) $this->db->where("value >", $item[0]);
+								if(!empty($item[1])) $this->db->where("value <", $item[1]);
+								$counter++;
+							}	
+							break;
+					}
+					$values = $this->_update_values($values);
+				}
+			}
+		}
+		
+		if($counter > 1)
+		{
+			$values = array_count_values($values);
+			foreach($values as $key => $counter)
+			{
+				if($counter > 1) $id[] = $key;
+			}
+		}
+		else
+		{
+			$id = $values;
+		}
+
+		if(empty($id))
+		{
+			return $content = array();
+		}
+		else
+		{
+			$this->db->where_in("id", $id);
+			$this->db->order_by($order, $direction); 
+			$query = $this->db->get($this->_table/*, $limit, $from*/);
+			$result = $query->result();
+			empty($result) ? $content = array() : $content = $result;
+			return $content;
+		}
+	}
+	
+	private function _update_values($values)
+	{
+		$query = $this->db->get('characteristics');
+		foreach($query->result_array() as $result)
+		{
+			$values[] = $result['object_id'];
+		}
+		return $values;
+	}
 }
