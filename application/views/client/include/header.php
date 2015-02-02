b1f44fdfbb702def4e4acc6acbb57eb3ed9ef812
luka-k <luka-rabota@yandex.ru>
2015-02-03 00:21:25 +0300

    шапка

--- application/views/client/include/header.php
+++ application/views/client/include/header.php
@@ -1,8 +1,11 @@
 <div class="col_12">
 	<div class="col_4">
-		<div class="col_12 cart">
+
+	</div>
+	<div class="col_4 clearfix">
+				<div class="col_12 cart">
 			<div class="col_4">
-				<a href="<?=base_url()?>cart" class="button">Корзина</a>
+				<a href="<?=base_url()?>cart" class="">Корзина</a>
 			</div>	
 			<div class="col_8">
 				В корзине <span id="total_qty"><?=$total_qty?></span> <?=$product_word?>.<br/>
@@ -20,8 +23,8 @@
 			</div>
 		<?else:?>
 			<form method="post" accept-charset="utf-8"  enctype="multipart/form-data" id="login" action="<?=base_url()?>registration/do_enter/"/>
-				<input type="text" name="login" class="col_12" placeholder="Логин"/></br></br>
-				<input type="password" name="password"  class="col_12" placeholder="Пароль"/></br></br>
+				<input type="text" name="login" class="col_5" placeholder="Логин"/>
+				<input type="password" name="password"  class="col_5" placeholder="Пароль"/>
 				<div class="col_6">
 					<a href="#" class="button small" onClick="document.forms['login'].submit()">Войти</a>
 				</div>
@@ -31,10 +34,5 @@
 			</form>
 			
 		<?endif;?>
-	</div>
-	<div class="col_4">
-		<form method="get" accept-charset="utf-8"  enctype="multipart/form-data" id="searchform" action="<?=base_url()?>search"/>
-			<input type="text" id="search_input" name="name" class="col_12 search" placeholder="Поиск" value="<?if(isset($search)):?><?=$search?><?endif;?>" onkeypress="autocomp()" autocomplete="off"/>
-		</form>
 	</div>
 </div>		
--- application/views/client/include/top-menu.php
+++ application/views/client/include/top-menu.php
@@ -2,4 +2,9 @@
 	<?foreach ($top_menu as $item):?>
 		<li <?if ($item[2] == 1):?> class="current"<?endif;?>><a href="<?=$item[1]?>"><?=$item[0]?></a></li>
 	<?endforeach;?>
+	<li class="right col_5">
+		<form method="get" accept-charset="utf-8"  enctype="multipart/form-data" id="searchform" action="<?=base_url()?>search"/>
+			<input type="text" id="search_input" name="name" class="col_12 search" placeholder="Поиск" value="<?if(isset($search)):?><?=$search?><?endif;?>" onkeypress="autocomp()" autocomplete="off"/>
+		</form>
+	</li>
 </ul>
