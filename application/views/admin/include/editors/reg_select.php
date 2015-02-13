682a739dd6ccb40b6cd7576abd87d3c8e0f08fb3
luka-k <luka-rabota@yandex.ru>
2015-02-14 01:22:00 +0300

    диллеры

--- application/views/admin/include/editors/reg_select.php
+++ application/views/admin/include/editors/reg_select.php
@@ -2,7 +2,7 @@
 	<label for="lbl_<?=$editors_counter?>" class="col_2">Регион</label>
 	<select id="lbl_<?=$editors_counter?>"  name="<?=$edit_name?>" class="col_9">
 		<option <?php if ($content->$edit_name == 0):?>selected<?php endif; ?> value="0">Не обходимо выбрать</option>
-		<?php foreach ($selects[$name] as $value =>$select): ?>
+		<?php foreach ($selects[$edit_name] as $value =>$select): ?>
 			<option class="option-1" value="<?=$value?>" <?php if ($content->$edit_name == $value):?>selected<?php endif; ?>>
 				<?=$select?>
 			</option>
