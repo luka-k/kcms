80903017aa2e20ffd0d672fe38cfddce5be0eef3
luka-k <luka-rabota@yandex.ru>
2015-02-28 23:15:21 +0300

    глюк рассылки

--- application/controllers/admin/mailouts_module.php
+++ application/controllers/admin/mailouts_module.php
@@ -38,60 +38,8 @@ class Mailouts_module extends Admin_Controller
 			'template' => $template,
 			'users_groups' => $this->users_groups->get_list(FALSE)
 		);
-		
-		if($action == "edit")
-		{
-			$this->load->view('admin/mailout', $data);	
-		}
-		elseif($action == "send")
-		{
-			$send_info = (object)$this->input->post();
 
-			$template_id = $send_info->template;
-
-			$info = new stdCLass();
-
-			$info->from_name = $send_info->from_name;
-			$info->from_email = $send_info->from_email;
-			$info->subject = $send_info->subject;
-			$info->message = $send_info->message;
-
-			
-			$this->db->where_in("id", $send_info->users_groups);
-			$query = $this->db->get('users_groups');
-			$users_groups = $query->result();
-			
-			$users = array();
-			foreach($users_groups as $group)
-			{
-				$users = array_merge($users, $this->users->group_list($group->id));
-			}
-			
-			$success = 0;
-			$no_success = 0;
-			foreach($users as $user)
-			{
-				$info->to = $user->email; 
-				
-				$parse_info['USER_NAME'] = $user->name;
-				
-				$is_send = $this->mailouts->send_mail($info, $parse_info);
-				
-				$is_send ? $success++ : $no_success++;
-			}
-			
-			$data = array(
-				"template_id" => $template_id,
-				"users_ids" => implode("/", $send_info->users_groups),
-				"mailouts_date" => date("Y-m-d"),
-				"success" => $success,
-				"no_success" => $no_success
-			);
-			
-			$this->mailouts->insert($data);
-			
-			redirect(base_url().'admin/mailouts_module/');
-		}
+		$this->load->view('admin/mailout', $data);	
 	}
 	
 	public function get_emails(){
@@ -114,7 +62,7 @@ class Mailouts_module extends Admin_Controller
 		$send_info->from_name =$info->post->from_name;
 		$send_info->from_email = $info->post->from_email;
 		$send_info->subject = $info->post->subject;
-		$send_info->message = urldecode($info->post->message);
+		$send_info->message = htmlspecialchars_decode(rawurldecode($info->post->message));
 		
 		$send_info->to = $info->user->email;
 		
