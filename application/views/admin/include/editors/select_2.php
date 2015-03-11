80e577efc64d5c293cc44b0a99fb905c809f92a7
luka-k <luka-rabota@yandex.ru>
2015-03-11 13:44:28 +0300

    кодировка отображении админки

--- application/views/admin/include/editors/select_2.php
+++ application/views/admin/include/editors/select_2.php
@@ -1,4 +1,4 @@
-﻿<div  class="col_12">
+<div  class="col_12">
 	<label for="select_<?=$name?>" class="col_3"><?=$edit[0]?></label>
 	<select id="select_<?=$name?>"  name="<?=$name?>" class="col_8">
 	
--- application/views/admin/include/editors/upload_image.php
+++ application/views/admin/include/editors/upload_image.php
@@ -1,4 +1,4 @@
-﻿<div class="col_12">
+<div class="col_12">
 	<div class="col_3">Добавить фотографии</div>
 	<div class="col_4"><input type="file" id="pic[]" name="pic" /></div>
 	<input type="hidden" name="upload_image" value="upload_image"/>
