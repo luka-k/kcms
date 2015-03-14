a8b22f7fa5d85b7598a2493fafd0829d71565512
luka-k <luka-rabota@yandex.ru>
2015-03-14 23:35:41 +0300

    get_sub_tree

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -386,14 +386,12 @@ class MY_Model extends CI_Model
 	public function get_sub_tree($parent_id, $parent_id_field)
 	{
 		$branches = $this->get_list(array($parent_id_field => $parent_id), FALSE, FALSE, "sort", "asc");
-		foreach($branches as $branch)
-		{
-			$branch->count_sub_products =(count($this->categories->get_sub_products($branch->id)));
-		}
-		$branches = $this->get_prepared_list($branches);
 
 		if ($branches) foreach ($branches as $i => $b)
 		{
+			if($this->_table == "categories") $b->count_sub_products =(count($this->categories->get_sub_products($b->id)));
+			$branches = $this->get_prepared_list($branches);
+		
 			$branches[$i]->childs = $this->get_sub_tree($b->id, $parent_id_field);
 			
 			if(!($this->url->set_active_class($this->url->active_branch, $branches[$i]))) $branches[$i]->class = "noactive";
