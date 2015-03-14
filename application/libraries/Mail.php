8ae5835afa1f0ab1e94a0f190c20ea09c1278d91
luka-k <luka-rabota@yandex.ru>
2015-03-15 00:43:15 +0300

    Mail.php

--- application/libraries/Mail.php
+++ application/libraries/Mail.php
@@ -6,33 +6,21 @@ class Mail {
 	{
 		$this->CI =& get_instance();
 		$this->CI->load->library('email');	
-		$this->CI->config->load('upload_config');
+		$this->CI->config->load('emails_config');
 	}
 
 	function send_mail($to, $subject, $message)
 	{
-		$config = array(
-			'protocol' => "mail",
-			'mailtype' => "html",
-			'wordwrap' => TRUE
-		);
-		$from = 'admin@admin.com';
-		$who = 'Admin';
+		$config = $this->CI->config->item('config');
+		$settings = $this->CI->settings->get_item(1);
 
 		$this->CI->email->initialize($config);
-		$this->CI->email->from($from, $who);
+		$this->CI->email->from($settings->admin_email, $settings->admin_name);
 		$this->CI->email->to($to);
 		$this->CI->email->subject($subject);
 		$this->CI->email->message($message);
-		if(!$this->CI->email->send())
-		{
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-
+		
+		return !$this->CI->email->send() ? FALSE : TRUE;
 	}
 
 }
