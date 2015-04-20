5d8c757f742a89e9e295b32650d1949e56dea5f6
luka-k <luka-rabota@yandex.ru>
2015-04-20 14:58:11 +0300

    сохраннения описания сайта в настройках

--- application/models/db/settings.php
+++ application/models/db/settings.php
@@ -31,11 +31,10 @@ class Settings extends MY_Model
 			'site_title' => array('Название сайта', 'text', 'trim|htmlspecialchars'),
 			'admin_email' => array('e-mail Администратора', 'text', 'trim|htmlspecialchars'),
 			'admin_name' => array('Имя Администратора', 'text', 'trim|htmlspecialchars'),
-			'site_description' => array('Описание сайта', 'tiny', 'trim'),
+			'site_description' => array('Описание сайта', 'tiny|htmlspecialchars', 'trim'),
 			'order_string' => array('Сообщение об оформлении заказа', 'text', 'trim|htmlspecialchars')
 		),
 		'SEO' => array(
-			'site_description' => array('Описание сайта', 'text'),
 			'site_keywords' => array('Ключевые слова', 'text'),
 			'lastmod' => array('lastmod', 'hidden')
 		),
