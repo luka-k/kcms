ade48ad0482874d440b536e99d1e24ab77edc143
luka-k <luka-rabota@yandex.ru>
2015-06-02 19:00:40 +0300

    config/characteristics

--- application/config/characteristics.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['view_type'] = array(
-	'text' => "Текстовое поле",
-	'multy' => "Множественный выбор",
-	'single' => "Одиночный выбор",
-	'interval' => "Интервал"
-);
