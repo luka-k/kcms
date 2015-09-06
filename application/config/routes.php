b70b9e06488aa53624aafea65d197e09335e20eb
luka-k <luka-rabota@yandex.ru>
2015-09-06 13:53:31 +0300

    test If-Modified-Since

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,6 +40,8 @@
 $route['default_controller'] = "index";
 $route['404_override'] = 'pages/page_404';
 
+$route['admin/get_database/test'] = "admin/get_database/test";
+$route['admin/get_database/(:any)'] = "admin/get_database/index/$1";
 $route['admin/(:any)'] = "admin/$1";
 $route['admin/(:any)/(:any)'] = "admin/$1/$2";
 
--- application/controllers/admin/get_database.php
+++ application/controllers/admin/get_database.php
@@ -8,11 +8,10 @@ class Get_database extends CI_Controller
 		parent::__construct();
 	}
 	
-	public function index()
-	{	
-		$school_name = $this->input->get('school_name');
-		$file_path = FCPATH."export/export_{$school_name}.sqlite";
-		
+	public function index($school_id)
+	{			
+		$file_path = FCPATH."export/export_{$school_id}.sqlite";
+
 		$modified_time = filemtime($file_path);
 		$modified_time_str = gmdate('D, d M Y H:i:s', $modified_time).' GMT';
 
@@ -26,5 +25,19 @@ class Get_database extends CI_Controller
 
 		header('Last-Modified: '.$modified_time_str);
 		echo file_get_contents($file_path);
+	}
+	
+	public function test(){
+		//$header = array('If-Modified-Since: '.date('D, d M Y H:i:s').' GMT');
+		$header = array('If-Modified-Since: Mon, 26 Jul 1997 00:00:00 GMT');
+		
+		if($curl = curl_init(base_url().'admin/get_database/shkola-1')) 
+		{
+			curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+			$out = curl_exec($curl);
+			echo $out;
+			curl_close($curl);
+		}
+
 	}
 }
