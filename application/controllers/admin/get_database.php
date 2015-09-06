b9a2a272c70b225ccb012fc87b49c80e48f31bc1
luka-k <luka-rabota@yandex.ru>
2015-09-06 03:02:28 +0300

    GET-запрос на получение базы

---
+++ application/controllers/admin/get_database.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Get_database extends CI_Controller 
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function index()
+	{	
+		$school_name = $this->input->get('school_name');
+		$file_path = FCPATH."export/export_{$school_name}.sqlite";
+		
+		$modified_time = filemtime($file_path);
+		$modified_time_str = gmdate('D, d M Y H:i:s', $modified_time).' GMT';
+
+		if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $modified_time)	
+		{
+			header('HTTP/1.1 304 Not Modified');
+			header('Last-Modified: '.$modified_time_str);
+			
+			exit();
+		}
+
+		header('Last-Modified: '.$modified_time_str);
+		echo file_get_contents($file_path);
+	}
+}
