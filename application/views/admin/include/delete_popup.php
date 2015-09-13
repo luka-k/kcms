e1ed0bc6d2a523e2caebfd1aea02ebd25aff9126
luka-k <luka-rabota@yandex.ru>
2015-09-13 21:13:48 +0300

    удалалить

--- application/views/admin/include/delete_popup.php
+++ application/views/admin/include/delete_popup.php
@@ -2,7 +2,7 @@
 <div id="delete_item" style="display:none;">
 	<div class="pop-up">
 		<div>
-			Вы точно уверены что хотите удалалить - <strong id="item_name"></strong>
+			Вы точно уверены что хотите удалить - <strong id="item_name"></strong>
 			<?if(isset($type) && $type == "categories"):?> и все связанные с ней товары и подкатегории<?endif;?>?
 		</div><br/>
 		<a href="" class="delete_button button small">Удалить?</a>
