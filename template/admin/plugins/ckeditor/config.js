a50921b0c45d48e91c6bf95e6850cb4ca4918c9c
luka-k <luka-rabota@yandex.ru>
2015-10-08 22:36:17 +0300

    конфиги редактора

--- template/admin/plugins/ckeditor/config.js
+++ template/admin/plugins/ckeditor/config.js
@@ -8,4 +8,11 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 	config.language = 'ru';
+	config.indentClasses = ["ul-grey", "ul-red", "text-red", "ul-content-red", "circle", "style-none", "decimal", "paragraph-portfolio-top", "ul-portfolio-top", "url-portfolio-top", "text-grey"];
+	config.protectedSource.push(/<(style)[^>]>.<\/style>/ig);
+	config.protectedSource.push(/<(script)[^>]>.<\/script>/ig);// разрешить теги <script>
+	config.protectedSource.push(/<(i)[^>]>.<\/i>/ig);// разрешить теги <i>
+	config.protectedSource.push(/<\?[\s\S]?\?>/g);// разрешить php-код
+	config.protectedSource.push(/<!--dev-->[\s\S]<!--\/dev-->/g);
+	config.allowedContent = true; / all tags /
 };
