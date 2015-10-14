92d2b92a26ab64c2bf8fe85327dc45bb3b2722c9
luka-k <luka-rabota@yandex.ru>
2015-10-14 16:50:48 +0300

    прокрутки в новостях

--- application/views/client/article.php
+++ application/views/client/article.php
@@ -57,8 +57,8 @@
 								
 								?>
 							<? if ($this->uri->segment(3) == 'novosti'): ?>
-						<div class="projects">
-							<ul class="projects__list-noscroll" style="margin-top: 15px;margin-left: 0;padding-left: 20px;list-style: none;width: 600px;">
+						<div class="projects" style="width:600px;">
+							<ul class="projects__list-noscroll clearfix" style="margin-top: 15px; margin-left: 20px; padding-left: 0px;list-style: none;width: 580px;">
 								<?for($i = 0; $i < count($object_images); $i+=3): $c = $object_images[$i];?>
 									<li class="projects__item projects-item" style="margin-left: 0; padding-left: 0;">
 										<?if(!empty($c->catalog_small_url)):
@@ -75,7 +75,7 @@
 												</a>
 												<? if ($i+2 < count($object_images)): $c = $object_images[$i+2];if ($c->caption)
 											$c->catalog_small_url=$c->video_url;?>
-											<a rel="nofollow" href="<?=base_url()?>popup_gallery/view?action=news&amp;nolinks=1&amp;product_id=<?=$content->article->id?>&amp;first_img=<?=$i+2?>&amp;title=<?=$content->article->name?>"  id="im_<?= $c->id?>" data-fancybox-type="iframe" class="projects-item__image-box modal-gallery-open" style="margin-left: 60px;">
+											<a rel="nofollow" href="<?=base_url()?>popup_gallery/view?action=news&amp;nolinks=1&amp;product_id=<?=$content->article->id?>&amp;first_img=<?=$i+2?>&amp;title=<?=$content->article->name?>"  id="im_<?= $c->id?>" data-fancybox-type="iframe" class="projects-item__image-box last modal-gallery-open" style="margin-left: 60px;">
 														<img src="<?=$c->catalog_small_url?>" id="project1" alt="project" class="projects-item__image hover-image" />
 													</a>
 												<? endif?>
