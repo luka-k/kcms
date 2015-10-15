45bf06e8edc95814e448c3b2a037686fe335f1f8
luka-k <luka-rabota@yandex.ru>
2015-10-15 18:02:38 +0300

    конфиг

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
-//$config['base_url']	= 'http://brightberry.ru';
+//$config['base_url']	= '';
+$config['base_url']	= 'http://brightberry.ru';
 
 /*
 |--------------------------------------------------------------------------
